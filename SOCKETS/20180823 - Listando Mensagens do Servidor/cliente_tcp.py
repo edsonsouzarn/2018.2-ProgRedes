a58ac8932ccecc62b58680162c628042b87a9d64
Charles Cesar Magno de Freitas <ccmfreitas@gmail.com>
2018-08-29 17:46:57 -0300

    Corrigindo o código

--- SOCKETS/20180823 - Listando Mensagens do Servidor/cliente_tcp.py
+++ SOCKETS/20180823 - Listando Mensagens do Servidor/cliente_tcp.py
@@ -1,7 +1,9 @@
+#!/usr/bin/python3
+
 import socket
 
-HOST = '10.20.1.171'
-PORT = 50000
+HOST = '10.20.1.6'
+PORT = 5001
 
 tcp_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
@@ -9,12 +11,14 @@ tcp_socket.connect((HOST, PORT))
 
 print('Para sair use CTRL+X\n')
 
-msg = input('Digite a mensagem: ')
+msg_received = ''
+msg_input = input('Digite a mensagem: ')
 
-while msg != '\x18':
-   msg = msg.encode('utf-8')
-   print(msg)
-   tcp_socket.send(msg)
-   msg = input('Digite a mensagem: ')
+while msg_input != '\x18':
+   msg_sended = msg_input.encode('utf-8')
+   tcp_socket.send(msg_sended)
+   msg_received = tcp_socket.recv(1024)
+   print(msg_received.decode('utf-8'))
+   msg_input = input('Digite a mensagem: ')
 
 tcp_socket.close()
--- SOCKETS/20180823 - Listando Mensagens do Servidor/servidor_tcp.py
+++ SOCKETS/20180823 - Listando Mensagens do Servidor/servidor_tcp.py
@@ -1,7 +1,9 @@
+#!/usr/bin/python3
+
 import socket
 
 HOST = ''
-PORT = 50000
+PORT = 5001
 
 tcp_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
@@ -11,12 +13,25 @@ tcp_socket.listen(1)
 print('Aguardando conexão')
 
 while True:
+   lista_msg_servidor = []
    con, cliente = tcp_socket.accept()
    print('Conectado por: ', cliente)
    while True:
-      msg = con.recvfrom(1024)
+      msg = con.recv(1024)
       if not msg: break
-      print(cliente, msg[0].decode('utf-8'))
-   
+      msg_serv = 'O cliente {0} disse: {1}'.format(cliente, msg.decode('utf-8'))
+      print(msg_serv)
+      lista_msg_servidor.append(msg_serv)
+      con.send(msg_serv.encode('utf-8'))
+
    print('Finalizando conexão do cliente ', cliente)
+
+
+   print(' ')
+   print('----------------------------------------------------------')
+   print('Historico das Mensagens do Cliente ',cliente)
+   for i in range(0, len(lista_msg_servidor)):
+       print(lista_msg_servidor[i])
+   print('----------------------------------------------------------')
+
    con.close()
